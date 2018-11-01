e650aa552641b0d098ce8294c7be6d9dbf2fdc7b
Sven <sven.hecht@gmail.com>
2018-11-01 23:12:04 +0100

    Improve rotational logic

--- content-engine/src/game/state/__tests__/reducers.spec.ts
+++ content-engine/src/game/state/__tests__/reducers.spec.ts
@@ -1,56 +1,89 @@
-import { Command } from '../../interpreter/types';
-import { executeCodeCommand } from '../actions';
-import { turtlePositionReducer, TurtleState } from '../reducers';
+import { Command } from "../../interpreter/types";
+import { executeCodeCommand } from "../actions";
+import { turtlePositionReducer, TurtleState } from "../reducers";
 
-describe('The Reducers', () => {
-    describe('turtlePositionReducer', () => {
-        const initialState: TurtleState = {
-            direction: 0,
-            position: {
-                x: 0,
-                y: 0,
-            },
-        };
+describe("The Reducers", () => {
+  describe("turtlePositionReducer", () => {
+    const initialState: TurtleState = {
+      direction: 0,
+      position: {
+        x: 0,
+        y: 0
+      }
+    };
 
-        it('rotates +90° on action right', () => {
-            const newState = turtlePositionReducer(initialState, executeCodeCommand(Command.Right));
-            expect(newState.direction * (180 / Math.PI)).toBeCloseTo(90);
-        });
+    const executeCommands = (commands: Command[]) => {
+      let result = initialState;
 
-        it('rotates -90° on action left', () => {
-            const newState = turtlePositionReducer(initialState, executeCodeCommand(Command.Left));
-            expect(newState.direction * (180 / Math.PI)).toBeCloseTo(-90);
-        });
+      commands.forEach(cmd => {
+        result = turtlePositionReducer(result, executeCodeCommand(cmd));
+      });
 
-        it('moves 1 in Y on action forward and not turned', () => {
-            const newState = turtlePositionReducer(initialState, executeCodeCommand(Command.Forward));
-            expect(newState.position.y).toBe(1);
-            expect(newState.position.x).toBe(0);
-        });
+      return result;
+    };
 
-        it('moves 1 in X on action forward and turned right', () => {
-            const newState1 = turtlePositionReducer(initialState, executeCodeCommand(Command.Right));
-            const newState2 = turtlePositionReducer(newState1, executeCodeCommand(Command.Forward));
-            expect(newState2.direction * 180 / Math.PI).toBe(90);
-            expect(newState2.position.x).toBe(1);
-            expect(newState2.position.y).toBe(0);
-        });
-
-        it('moves -1 in X on action forward and turned left', () => {
-            const newState1 = turtlePositionReducer(initialState, executeCodeCommand(Command.Left));
-            const newState2 = turtlePositionReducer(newState1, executeCodeCommand(Command.Forward));
-            expect(newState2.direction * 180 / Math.PI).toBe(-90);
-            expect(newState2.position.x).toBe(-1);
-            expect(newState2.position.y).toBe(0);
-        });
-
-        it('moves -1 in Y on action forward and turned left twice', () => {
-            const newState1 = turtlePositionReducer(initialState, executeCodeCommand(Command.Left));
-            const newState2 = turtlePositionReducer(newState1, executeCodeCommand(Command.Left));
-            const newState3 = turtlePositionReducer(newState2, executeCodeCommand(Command.Forward));
-            expect(newState3.direction * 180 / Math.PI).toBe(-90);
-            expect(newState3.position.x).toBe(0);
-            expect(newState3.position.y).toBe(-1);
-        });
+    it("rotates +90° on action right", () => {
+      const newState = turtlePositionReducer(
+        initialState,
+        executeCodeCommand(Command.Right)
+      );
+      expect(newState.direction * (180 / Math.PI)).toBeCloseTo(90);
     });
+
+    it("rotates -90° on action left", () => {
+      const newState = turtlePositionReducer(
+        initialState,
+        executeCodeCommand(Command.Left)
+      );
+      expect(newState.direction * (180 / Math.PI)).toBeCloseTo(-90);
+    });
+
+    it("rotates -180° on action left twice", () => {
+      const newState = executeCommands([Command.Left, Command.Left]);
+      expect(newState.direction * (180 / Math.PI)).toBeCloseTo(-180);
+    });
+
+    it.skip("rotates 180° on action right twice", () => {
+      const newState = executeCommands([Command.Right, Command.Right]);
+      expect(newState.direction * (180 / Math.PI)).toBeCloseTo(180);
+    });
+
+    it("moves 1 in Y on action forward and not turned", () => {
+      const newState = turtlePositionReducer(
+        initialState,
+        executeCodeCommand(Command.Forward)
+      );
+      expect(newState.position.y).toBe(1);
+      expect(newState.position.x).toBe(0);
+    });
+
+    it("moves 1 in X on action forward and turned right", () => {
+      const newState = executeCommands([Command.Right, Command.Forward]);
+      expect((newState.direction * 180) / Math.PI).toBe(90);
+      expect(newState.position.x).toBe(1);
+      expect(newState.position.y).toBe(0);
+    });
+
+    it("moves -1 in X on action forward and turned left", () => {
+      const newState = executeCommands([Command.Left, Command.Forward]);
+      expect((newState.direction * 180) / Math.PI).toBe(-90);
+      expect(newState.position.x).toBe(-1);
+      expect(newState.position.y).toBe(0);
+    });
+
+    it("moves -1 in Y on action forward and turned left twice", () => {
+      const newState = executeCommands([
+        Command.Left,
+        Command.Left,
+        Command.Forward
+      ]);
+
+      expect(newState.direction).toBe(-Math.PI);
+      expect(Math.sin(newState.direction)).toBeCloseTo(0);
+      expect(Math.cos(newState.direction)).toBeCloseTo(-1);
+      expect((newState.direction * 180) / Math.PI).toBe(-180);
+      expect(newState.position.y).toBe(-1);
+      expect(newState.position.x).toBe(-0);
+    });
+  });
 });
--- content-engine/src/game/state/reducers.ts
+++ content-engine/src/game/state/reducers.ts
@@ -1,73 +1,81 @@
-import { combineReducers } from 'redux';
-import { ActionType, getType, StateType } from 'typesafe-actions';
+import { combineReducers } from "redux";
+import { ActionType, getType, StateType } from "typesafe-actions";
 
-import { StatementInstance } from '../../types';
-import { Command } from '../interpreter/types';
+import { StatementInstance } from "../../types";
+import { Command } from "../interpreter/types";
 
 export interface TurtleState {
-    position: {
-        x: number,
-        y: number,
-    };
-    direction: number;
+  position: {
+    x: number;
+    y: number;
+  };
+  direction: number;
 }
 
 const initialTurtleState = {
-    direction: 0,
-    position: { x: 0, y: 0 },
+  direction: 0,
+  position: { x: 0, y: 0 }
 };
 
-export const activeStatements = (state: StatementInstance[] = [], action: GameAction): StatementInstance[] => {
-    switch (action.type) {
-        case getType(actions.updateStatement):
-            return [...state, action.payload];
+export const activeStatements = (
+  state: StatementInstance[] = [],
+  action: GameAction
+): StatementInstance[] => {
+  switch (action.type) {
+    case getType(actions.updateStatement):
+      return [...state, action.payload];
 
-        case getType(actions.removeStatement):
-            return state.filter((statement) => statement.id === action.payload.id);
+    case getType(actions.removeStatement):
+      return state.filter(statement => statement.id === action.payload.id);
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 };
 
 export const turtlePositionReducer = (
-    state: TurtleState = initialTurtleState,
-    action: GameAction,
+  state: TurtleState = initialTurtleState,
+  action: GameAction
 ): TurtleState => {
-    switch (action.type) {
-        case getType(actions.executeCodeCommand):
-            let direction = state.direction;
-            let position = state.position;
+  switch (action.type) {
+    case getType(actions.executeCodeCommand):
+      let direction = state.direction;
+      let position = state.position;
 
-            switch (action.payload) {
-                case Command.Forward:
-                    position = {
-                        x: Math.floor(position.x + Math.sin(direction)),
-                        y: Math.floor(position.y + Math.cos(direction)),
-                    };
-                    break;
-                case Command.Left:
-                    direction -= Math.PI / 2;
-                    break;
-                case Command.Right:
-                    direction += Math.PI / 2;
-                    break;
-            }
-            direction = direction % Math.PI;
-            return {
-                direction,
-                position,
-            };
+      switch (action.payload) {
+        case Command.Forward:
+          position = {
+            x: Math.round(position.x + Math.sin(direction)),
+            y: Math.round(position.y + Math.cos(direction))
+          };
+          break;
+        case Command.Left:
+          direction -= Math.PI / 2;
+          break;
+        case Command.Right:
+          direction += Math.PI / 2;
+          break;
+      }
 
-        default:
-            return state;
-    }
+      direction = (direction + Math.PI) % (Math.PI * 2);
+      if (direction < 0) {
+        direction += Math.PI;
+      }
+      direction -= Math.PI;
+      return {
+        direction,
+        position
+      };
+
+    default:
+      return state;
+  }
 };
 
 export const gameReducer = combineReducers({
-    activeStatements,
+  activeStatements
 });
 
-import * as actions from './actions';
+import * as actions from "./actions";
 export type GameAction = ActionType<typeof actions>;
 export type GameState = StateType<typeof gameReducer>;
