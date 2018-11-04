282eaee1d29deb3a860c1d0a2516763850ab5c4c
Sven <sven.hecht@gmail.com>
2018-11-04 01:31:54 +0100

    Add connection logic

--- content-engine/src/game/turtle-controller.ts
+++ content-engine/src/game/turtle-controller.ts
@@ -1,27 +1,72 @@
 import { Store } from "redux";
 import { getTurtleMarkerPosition, getTurtlePosition } from "./state/selectors";
 
+const MOVEMENT_DURATION = 200;
+
+enum TurtleDirection {
+  Stop = "0",
+  TurnLeftWheel = "1",
+  TurnRightWheel = "2",
+  Forward = "3"
+}
+
 export default class TurtleController {
   private store: Store;
+  private connection?: WebSocket;
+  private turtleHostname: string;
 
-  constructor(store: Store) {
+  constructor(store: Store, turtleHostname: string) {
     this.store = store;
+    this.turtleHostname = turtleHostname;
 
     store.subscribe(this.onStateChange.bind(this));
   }
 
+  public connect() {
+    this.connection = new WebSocket(`ws://${this.turtleHostname}:81/`, [
+      "arduino"
+    ]);
+    this.connection.onopen = () => {
+      if (this.connection) {
+        this.connection.send("Connect " + new Date());
+      }
+    };
+    this.connection.onerror = error => {
+      console.log("WebSocket Error ", error);
+    };
+    this.connection.onmessage = e => {
+      console.log("Server: ", e.data);
+    };
+    this.connection.onclose = () => {
+      console.log("WebSocket connection closed");
+    };
+  }
+
   private onStateChange() {
+    if (!this.connection) {
+      return;
+    }
+
     const state = this.store.getState();
     const targetPosition = getTurtlePosition(state);
-    const actualPosition = getTurtleMarkerPosition(state);
+    const currentPosition = getTurtleMarkerPosition(state);
+    // const currentRotation = getTurtleMarkerRotation(state);
 
-    if (actualPosition) {
+    if (currentPosition) {
       const movementVector = [
-        targetPosition.x - actualPosition[0],
-        targetPosition.y - actualPosition[1]
+        targetPosition.x - currentPosition[0],
+        targetPosition.y - currentPosition[1]
       ];
 
       // TODO transmit movement command to turtle
+    }
+  }
+
+  private sendMovement(direction: TurtleDirection) {
+    if (this.connection) {
+      const commandstr = `#${direction}${MOVEMENT_DURATION.toString(16)}`;
+      console.log(`Sending movement command: ${commandstr}`);
+      this.connection.send(commandstr);
     }
   }
 }
