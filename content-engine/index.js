cd88051acdcb4199ae3e016f11d3bd286299e65e
Sven <sven.hecht@gmail.com>
2019-01-18 17:17:02 +0100

    start chrome fullscreen

--- content-engine/index.js
+++ content-engine/index.js
@@ -1,38 +1,34 @@
-const puppeteer = require('puppeteer');
-const express = require('express');
-const http = require('http');
-const WebSocketServer = require('ws').Server;
+const puppeteer = require("puppeteer");
+const express = require("express");
+const http = require("http");
+const WebSocketServer = require("ws").Server;
 
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 const server = http.createServer(app).listen(3000, async () => {
-  console.log('Example app listening on port 3000!');
+  console.log("Example app listening on port 3000!");
 
-  console.log('Starting automation');
-  const width = 1024;
-  const height = 768;
+  console.log("Starting automation");
   const browser = await puppeteer.launch({
     headless: false,
-    args: [
-      `--window-size=${width},${height}`
-    ],
+    args: [`--start-fullscreen`]
   });
   const page = await browser.newPage();
-  await page.goto('http://localhost:3000/');
-  await page.screenshot({ path: 'example.png' });
+  await page.goto("http://localhost:3000/");
+  await page.screenshot({ path: "example.png" });
 
   //await browser.close();
 
-  console.log('Done');
+  console.log("Done");
 });
 
-console.log('Starting websocket server');
+console.log("Starting websocket server");
 const wss = new WebSocketServer({
   server: server
 });
 
-wss.on('connection', (ws, req) => {
-  console.log('WS Connection reveiced');
+wss.on("connection", (ws, req) => {
+  console.log("WS Connection reveiced");
 });
