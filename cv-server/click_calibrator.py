adc4e979b8e7bdfa9e1d4e94c4e4d1e0fc064c7d
Sebastian <sebastian.hoefer@gmail.com>
2019-01-18 22:42:46 +0100

    more testing

--- cv-server/click_calibrator.py
+++ cv-server/click_calibrator.py
@@ -22,6 +22,17 @@ def get_homography():
         )
 	print("Transformation: ", homography)
 
+def show_image(image):
+	print('showing image  ', image.shape)
+	print('homography: ', homography)	
+	# flags = cv2.INTER_LINEAR + 8 #(CV_WARP_FILL_OUTLIERS) # + 16 #(CV_WARP_INVERSE_MAP)
+	# canvas = cv2.warpPerspective( 
+		# image, 
+		# homography if show_warped else numpy.eye(3, 3),
+		# (1920, 1080)
+	# )
+	cv2.imshow("image", image)
+
 def click_and_store(event, x, y, flags, param):
 	# grab references to the global variables
 	global all_pts, current_pt_nr, current_pts
@@ -39,45 +50,52 @@ def click_and_store(event, x, y, flags, param):
 			current_pts = []
 			get_homography()
 
-		cv2.imshow("image", image)
+		# cv2.imshow("image", image)
+		show_image(image)
 
 	# check to see if the left mouse button was released
 	# elif event == cv2.EVENT_LBUTTONUP:
 
-def show_image(image):
-	print('showing image')
-	print('homography: ', homography)
-	flags = cv2.INTER_LINEAR + 8 #(CV_WARP_FILL_OUTLIERS) # + 16 #(CV_WARP_INVERSE_MAP)
-	canvas = cv2.warpPerspective( 
-		image, 
-		homography,
-		(800, 600)
-	)
-	cv2.imshow("image", canvas)
-
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="Path to the image")
+# ap.add_argument("-i", "--image", required=True, help="Path to the image")
 args = vars(ap.parse_args())
 
 # load the image, clone it, and setup the mouse callback function
-image = cv2.imread(args["image"])
-clone = image.copy()
-cv2.namedWindow("image")
-cv2.setMouseCallback("image", click_and_store)
+# image = cv2.imread(args["image"])
+# clone = image.copy()
+
+# image = numpy.array((1080, 1920, 3), dtype=numpy.uint8)
+
+
+wnd_name = 'image'
+cv2.namedWindow(wnd_name, 0)
+cv2.resizeWindow(wnd_name, 1920, 1080)
+cv2.setWindowProperty(wnd_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+# cv2.moveWindow( self.wnd_name, self.pos_x+self.offset_x , self.pos_y+self.offset_y )
+
+# cv2.setMouseCallback(wnd_name, click_and_store)
+
+cap = cv2.VideoCapture(1)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+ret, image = cap.read()
+
 
 # keep looping until the 'q' key is pressed
 while True:
+
 	# display the image and wait for a keypress
 	show_image(image)
 	key = '%c' % (cv2.waitKey(0) & 0xFF)
 
 	# if the 'r' key is pressed, reset the cropping region
-	if key is 'r':
-		image = clone.copy()
+	# if key is 'r':
+		# image = clone.copy()
 
 	# toggle between warped and unwarped projection
-	elif key is 'w':
+	if key is 'w':
 		show_warped = not show_warped
 
 	# if the 'q' key is pressed, break from the loop
@@ -86,10 +104,10 @@ while True:
 
 # if there are two reference points, then crop the region of interest
 # from teh image and display it
-if len(all_pts) == 2:
-	roi = clone[all_pts[0][1]:all_pts[1][1], all_pts[0][0]:all_pts[1][0]]
-	cv2.imshow("ROI", roi)
-	cv2.waitKey(0)
+# if len(all_pts) == 2:
+	# roi = clone[all_pts[0][1]:all_pts[1][1], all_pts[0][0]:all_pts[1][0]]
+	# cv2.imshow("ROI", roi)
+	# cv2.waitKey(0)
 
 # close all open windows
 cv2.destroyAllWindows()
