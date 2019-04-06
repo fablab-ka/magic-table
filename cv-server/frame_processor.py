Fix go to target rotation

cv-server/frame_processor.py
@@ -11,12 +11,12 @@ pixel_per_meter = 100/0.102
 
 
 class FrameProcessor:
-    def __init__(self, calibration):
-        self.camera_index = 2
+    def __init__(self, calibration: dict, camera_index: int = 0, debug_mode: bool = False):
+        self.camera_index = camera_index
         self.marker_length_in_meter = 0.04
         self.projector_to_camera_offset = np.array([0, 0, 0])
 
-        self.debug = True
+        self.debug = debug_mode
 
         self.dist_coeffs = calibration['dist_coeffs']
         self.camera_matrix = calibration['camera_matrix']
@@ -33,6 +33,8 @@ class FrameProcessor:
 
     def init_camera(self):
         self.cap = cv2.VideoCapture(self.camera_index)
+        if self.cap is None or not self.cap.isOpened():
+            raise RuntimeError(f"Unable to open video source! (camera index: {self.camera_index})")
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 

cv-server/main.py
@@ -12,6 +12,9 @@ clients = []
 server = None
 server_running = False
 
+debug = False
+cam_index = 1
+
 
 def send_transforms(clients, transforms):
     message = json.dumps(transforms)
@@ -23,7 +26,7 @@ def start_camera_analysis():
     print("Starting Camera Analysis")
 
     calibration = np.load('./calibration_1080.npz')
-    frame_processor = FrameProcessor(calibration)
+    frame_processor = FrameProcessor(calibration, camera_index=cam_index, debug_mode=debug)
     frame_processor.init_camera()
 
     while server_running:
@@ -66,11 +69,12 @@ def run_server():
 
     server_running = True
 
-    analysis_thread = threading.Thread(target=start_camera_analysis)
+    analysis_thread = threading.Thread(target=start_camera_analysis, daemon=True)
     analysis_thread.start()
 
     try:
-        server.serveforever()
+        while analysis_thread.isAlive():
+            server.serveonce()
     except KeyboardInterrupt:
         server_running = False
         analysis_thread.join()